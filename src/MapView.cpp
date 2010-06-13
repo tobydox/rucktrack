tobydox/rucktrack

commit 1759715fc3c890d036cf0e1f7b4eb773dfcf7d28

New abstraction class MapProvider and implementation GoogleMapsProvider
Technology details of the used map provider (which currently is only
GoogleMaps) have been abstracted in an own class MapProvider.
Most of the implementation of the MapView class has been moved into the
new GoogleMapsProvider class.

This allows for easily adding support for other map providers later,
e.g. OSM.

src/MapView.cpp
@@ -1,7 +1,7 @@
 /*
  * MapView.cpp - implementation of MapView class
  *
- * Copyright (c) 2009 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ * Copyright (c) 2009-2010 Tobias Doerffel <tobydox/at/users.sourceforge.net>
  *
  * This file is part of RuckTrack - http://rucktrack.sourceforge.net
  *
@@ -22,16 +22,17 @@
  *
  */
 
-#include <QtCore/QDebug>
 #include <QtWebKit/QWebFrame>
 
+#include "MapProvider.h"
 #include "MapView.h"
 
 #define evalJS(x) page()->mainFrame()->evaluateJavaScript(x)
 
 
 MapView::MapView( QWidget * _parent ) :
-	QWebView( _parent )
+	QWebView( _parent ),
+	m_mapProvider( NULL )
 {
 	page()->mainFrame()->addToJavaScriptWindowObject( "mapView", this );
 	setUrl( QUrl( "qrc:/resources/googlemaps.html" ) );
@@ -40,71 +41,27 @@ MapView::MapView( QWidget * _parent ) :
 
 
 
+MapView::~MapView()
+{
+	delete m_mapProvider;
+}
+
+
+
+
+void MapView::setMapProvider( MapProvider * mapProvider )
+{
+	m_mapProvider = mapProvider;
+}
+
+
+
+
 void MapView::showRoute( const Route & _route )
 {
-	page()->mainFrame()->addToJavaScriptWindowObject( "mapView", this );
-	QString polyline;
-	QStringList markers;
-	const double invalidVal = -1000;
-	double minLat = invalidVal;
-	double minLon = invalidVal;
-	double maxLat = invalidVal;
-	double maxLon = invalidVal;
-
-	foreach( const TrackSegment & seg, _route )
+	if( mapProvider() )
 	{
-		bool first = true;
-		foreach( const TrackPoint & pt, seg )
-		{
-			if( minLat <= invalidVal || pt.latitude() < minLat )
-			{
-				minLat = pt.latitude();
-			}
-			if( minLon <= invalidVal || pt.longitude() < minLon )
-			{
-				minLon = pt.longitude();
-			}
-			if( maxLat <= invalidVal || pt.latitude() > maxLat )
-			{
-				maxLat = pt.latitude();
-			}
-			if( maxLon <= invalidVal || pt.longitude() > maxLon )
-			{
-				maxLon = pt.longitude();
-			}
-			QString pointStr = QString( "new GLatLng(%1,%2)").
-					arg( pt.latitude() ).arg( pt.longitude() );
-			if( first )
-			{
-				markers += pointStr;
-				first = false;
-			}
-			if( !polyline.isEmpty() )
-			{
-				polyline += ",";
-			}
-			polyline += pointStr;
-		}
-	}
-
-	evalJS( "map.clearOverlays()" );
-	evalJS( QString( "var polyline = new GPolyline([%1], \"#ff0000\", 4)" ).arg( polyline ) );
-	evalJS( "map.addOverlay(polyline)" );
-	evalJS( "GEvent.addListener(map, \"click\", function(overlay,latlng) { mapView.selectPoint(latlng.lat(),latlng.lng()); });" );
-	evalJS( "GEvent.addListener(polyline, \"click\", function(latlng) { mapView.selectPoint(latlng.lat(),latlng.lng()); });" );
-	foreach( const QString & marker, markers )
-	{
-		evalJS( QString( "map.addOverlay(new GMarker(%1))" ).arg( marker ) );
-	}
-	if( minLat > invalidVal && minLon > invalidVal &&
-		maxLat > invalidVal && maxLat > invalidVal )
-	{
-		evalJS( QString( "var bounds = new GLatLngBounds;"
-					"bounds.extend(new GLatLng(%1,%2));"
-					"bounds.extend(new GLatLng(%3,%4));" ).
-					arg( minLat ).arg( minLon ).
-					arg( maxLat ).arg( maxLon ) );
-		evalJS( "map.setCenter(bounds.getCenter(), map.getBoundsZoomLevel(bounds))" );
+		mapProvider()->showRoute( _route );
 	}
 }
 
@@ -113,17 +70,9 @@ void MapView::showRoute( const Route & _route )
 
 void MapView::highlightPoint( double _lat, double _lon )
 {
-	evalJS( "map.removeOverlay(pointMarker)" );
-	evalJS( QString( "pointMarker = new GMarker(new GLatLng(%1, %2), markerOptions);").
-				arg( _lat ).arg( _lon ) );
-	evalJS( "map.addOverlay(pointMarker)" );
-}
-
-
-
-
-void MapView::selectPoint( double _lat, double _lon )
-{
-	emit clickedPoint( _lat, _lon );
+	if( mapProvider() )
+	{
+		mapProvider()->highlightPoint( _lat, _lon );
+	}
 }
 

CMakeLists.txt
@@ -40,7 +40,9 @@ SET(CMAKE_CXX_FLAGS "-O2 -g -fno-exceptions -Wall ${CMAKE_CXX_FLAGS}")
 
 SET(rucktrack_SOURCES src/main.cpp
 			src/RTMainWindow.cpp
+			src/GoogleMapsProvider.cpp
 			src/GpxFile.cpp
+			src/MapProvider.cpp
 			src/MapView.cpp
 			src/PlotView.cpp
 			src/RouteTableModel.cpp
@@ -65,7 +67,9 @@ SET(rucktrack_SOURCES src/main.cpp
 
 SET(rucktrack_INCLUDES src/RTMainWindow.h
 			src/AboutDialog.h
+			src/GoogleMapsProvider.h
 			src/GpxFile.h
+			src/MapProvider.h
 			src/MapView.h
 			src/PlotView.h
 			src/ProgressTrackingNetworkAccessManager.h

src/GoogleMapsProvider.cpp
@@ -1,0 +1,132 @@
+/*
+ * GoogleMapsProvider.cpp - implementation of GoogleMapsProvider class
+ *
+ * Copyright (c) 2010 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ *
+ * This file is part of RuckTrack - http://rucktrack.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include <QtCore/QDebug>
+#include <QtWebKit/QWebFrame>
+
+#include "GoogleMapsProvider.h"
+
+#define evalJS(x) webFrame()->evaluateJavaScript(x)
+
+
+GoogleMapsProvider::GoogleMapsProvider( QWebFrame * _parent ) :
+	MapProvider( _parent )
+{
+}
+
+
+
+
+GoogleMapsProvider::~GoogleMapsProvider()
+{
+}
+
+
+
+
+void GoogleMapsProvider::showRoute( const Route & _route )
+{
+	webFrame()->addToJavaScriptWindowObject( "mapProvider", this );
+	QString polyline;
+	QStringList markers;
+	const double invalidVal = -1000;
+	double minLat = invalidVal;
+	double minLon = invalidVal;
+	double maxLat = invalidVal;
+	double maxLon = invalidVal;
+
+	foreach( const TrackSegment & seg, _route )
+	{
+		bool first = true;
+		foreach( const TrackPoint & pt, seg )
+		{
+			if( minLat <= invalidVal || pt.latitude() < minLat )
+			{
+				minLat = pt.latitude();
+			}
+			if( minLon <= invalidVal || pt.longitude() < minLon )
+			{
+				minLon = pt.longitude();
+			}
+			if( maxLat <= invalidVal || pt.latitude() > maxLat )
+			{
+				maxLat = pt.latitude();
+			}
+			if( maxLon <= invalidVal || pt.longitude() > maxLon )
+			{
+				maxLon = pt.longitude();
+			}
+			QString pointStr = QString( "new GLatLng(%1,%2)").
+					arg( pt.latitude() ).arg( pt.longitude() );
+			if( first )
+			{
+				markers += pointStr;
+				first = false;
+			}
+			if( !polyline.isEmpty() )
+			{
+				polyline += ",";
+			}
+			polyline += pointStr;
+		}
+	}
+
+	evalJS( "map.clearOverlays()" );
+	evalJS( QString( "var polyline = new GPolyline([%1], \"#ff0000\", 4)" ).arg( polyline ) );
+	evalJS( "map.addOverlay(polyline)" );
+	evalJS( "GEvent.addListener(map, \"click\", function(overlay,latlng) { mapProvider.selectPoint(latlng.lat(),latlng.lng()); });" );
+	evalJS( "GEvent.addListener(polyline, \"click\", function(latlng) { mapProvider.selectPoint(latlng.lat(),latlng.lng()); });" );
+	foreach( const QString & marker, markers )
+	{
+		evalJS( QString( "map.addOverlay(new GMarker(%1))" ).arg( marker ) );
+	}
+	if( minLat > invalidVal && minLon > invalidVal &&
+		maxLat > invalidVal && maxLat > invalidVal )
+	{
+		evalJS( QString( "var bounds = new GLatLngBounds;"
+					"bounds.extend(new GLatLng(%1,%2));"
+					"bounds.extend(new GLatLng(%3,%4));" ).
+					arg( minLat ).arg( minLon ).
+					arg( maxLat ).arg( maxLon ) );
+		evalJS( "map.setCenter(bounds.getCenter(), map.getBoundsZoomLevel(bounds))" );
+	}
+}
+
+
+
+
+void GoogleMapsProvider::highlightPoint( double _lat, double _lon )
+{
+	evalJS( "map.removeOverlay(pointMarker)" );
+	evalJS( QString( "pointMarker = new GMarker(new GLatLng(%1, %2), markerOptions);").
+				arg( _lat ).arg( _lon ) );
+	evalJS( "map.addOverlay(pointMarker)" );
+}
+
+
+
+QUrl GoogleMapsProvider::mapUrl() const
+{
+	return QUrl( "qrc:/resources/googlemaps.html" );
+}

src/GoogleMapsProvider.h
@@ -1,7 +1,7 @@
 /*
- * MapView.h - header file for MapView class
+ * GoogleMapsProvider.h - a MapProvider for GoogleMaps
  *
- * Copyright (c) 2009-2010 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ * Copyright (c) 2010 Tobias Doerffel <tobydox/at/users.sourceforge.net>
  *
  * This file is part of RuckTrack - http://rucktrack.sourceforge.net
  *
@@ -22,35 +22,24 @@
  *
  */
 
-#ifndef _MAP_VIEW_H
-#define _MAP_VIEW_H
+#ifndef _GOOGLE_MAPS_PROVIDER_H
+#define _GOOGLE_MAPS_PROVIDER_H
 
-#include <QtWebKit/QWebView>
+#include "MapProvider.h"
 
-#include "Route.h"
 
-class MapProvider;
-
-class MapView : public QWebView
+class GoogleMapsProvider : public MapProvider
 {
 public:
-	MapView( QWidget * _parent );
-	virtual ~MapView();
+	GoogleMapsProvider( QWebFrame * _parent );
+	virtual ~GoogleMapsProvider();
 
-	void setMapProvider( MapProvider * mapProvider );
-	MapProvider * mapProvider()
-	{
-		return m_mapProvider;
-	}
+	virtual void showRoute( const Route & _route );
+	virtual void highlightPoint( double _lat, double _lon );
 
-	void showRoute( const Route & _route );
-	void highlightPoint( double _lat, double _lon );
-
-
-private:
-	MapProvider * m_mapProvider;
+	virtual QUrl mapUrl() const;
 
 } ;
 
 
-#endif // _MAP_VIEW_H
+#endif // _GOOGLE_MAPS_PROVIDER_H

src/MapProvider.cpp
@@ -1,7 +1,7 @@
 /*
- * MapView.h - header file for MapView class
+ * MapProvider.cpp - implementation of abstrat MapProvider class
  *
- * Copyright (c) 2009-2010 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ * Copyright (c) 2010 Tobias Doerffel <tobydox/at/users.sourceforge.net>
  *
  * This file is part of RuckTrack - http://rucktrack.sourceforge.net
  *
@@ -22,35 +22,29 @@
  *
  */
 
-#ifndef _MAP_VIEW_H
-#define _MAP_VIEW_H
+#include <QtWebKit/QWebFrame>
 
-#include <QtWebKit/QWebView>
+#include "MapProvider.h"
 
-#include "Route.h"
 
-class MapProvider;
-
-class MapView : public QWebView
+MapProvider::MapProvider( QWebFrame * _parent ) :
+	QObject( _parent ),
+	m_webFrame( _parent )
 {
-public:
-	MapView( QWidget * _parent );
-	virtual ~MapView();
-
-	void setMapProvider( MapProvider * mapProvider );
-	MapProvider * mapProvider()
-	{
-		return m_mapProvider;
-	}
-
-	void showRoute( const Route & _route );
-	void highlightPoint( double _lat, double _lon );
+}
 
 
-private:
-	MapProvider * m_mapProvider;
-
-} ;
 
 
-#endif // _MAP_VIEW_H
+MapProvider::~MapProvider()
+{
+}
+
+
+
+
+void MapProvider::selectPoint( double _lat, double _lon )
+{
+	emit clickedPoint( _lat, _lon );
+}
+

src/MapProvider.h
@@ -1,7 +1,7 @@
 /*
- * MapView.h - header file for MapView class
+ * MapProvider.h - header file for MapProvider class
  *
- * Copyright (c) 2009-2010 Tobias Doerffel <tobydox/at/users.sourceforge.net>
+ * Copyright (c) 2010 Tobias Doerffel <tobydox/at/users.sourceforge.net>
  *
  * This file is part of RuckTrack - http://rucktrack.sourceforge.net
  *
@@ -22,35 +22,49 @@
  *
  */
 
-#ifndef _MAP_VIEW_H
-#define _MAP_VIEW_H
+#ifndef _MAP_PROVIDER_H
+#define _MAP_PROVIDER_H
 
-#include <QtWebKit/QWebView>
+#include <QtCore/QObject>
+#include <QtCore/QUrl>
 
 #include "Route.h"
 
-class MapProvider;
+class QWebFrame;
 
-class MapView : public QWebView
+class MapProvider : public QObject
 {
+	Q_OBJECT
 public:
-	MapView( QWidget * _parent );
-	virtual ~MapView();
+	MapProvider( QWebFrame * _parent );
+	virtual ~MapProvider();
 
-	void setMapProvider( MapProvider * mapProvider );
-	MapProvider * mapProvider()
+	// pure virtual functions to be implemented by subclasses
+	virtual void showRoute( const Route & _route ) = 0;
+	virtual void highlightPoint( double _lat, double _lon ) = 0;
+
+	virtual QUrl mapUrl() const = 0;
+
+
+public slots:
+	virtual void selectPoint( double _lat, double _lon );
+
+
+protected:
+	QWebFrame * webFrame()
 	{
-		return m_mapProvider;
+		return m_webFrame;
 	}
-
-	void showRoute( const Route & _route );
-	void highlightPoint( double _lat, double _lon );
 
 
 private:
-	MapProvider * m_mapProvider;
+	QWebFrame * m_webFrame;
+
+
+signals:
+	void clickedPoint( double _lat, double _lon );
 
 } ;
 
 
-#endif // _MAP_VIEW_H
+#endif // _MAP_PROVIDER_H

src/RTMainWindow.cpp
@@ -27,6 +27,7 @@
 #include <QtGui/QProgressBar>
 
 #include "AboutDialog.h"
+#include "GoogleMapsProvider.h"
 #include "GpxFile.h"
 #include "RTMainWindow.h"
 #include "ProgressTrackingNetworkAccessManager.h"
@@ -70,12 +71,17 @@ RTMainWindow::RTMainWindow(QWidget *parent) :
 	connect( nam, SIGNAL( progressChanged( int ) ),
 				webPageProgress, SLOT( setValue( int ) ) );
 
+	// install GoogleMapsProvider
+	MapProvider * mapProvider =  new GoogleMapsProvider(
+										ui->mapView->page()->mainFrame() );
+	ui->mapView->setMapProvider( mapProvider );
+
 	// setup TrackPointsView
 	ui->trackPointsView->setModel( m_routeTableModel );
 	connect( ui->trackPointsView->selectionModel(),
 				SIGNAL( currentChanged( const QModelIndex &, const QModelIndex & ) ),
 				this, SLOT( highlightSelectedTrackPoint( const QModelIndex &, const QModelIndex & ) ) );
-	connect( ui->mapView, SIGNAL( clickedPoint( double, double ) ),
+	connect( mapProvider, SIGNAL( clickedPoint( double, double ) ),
 				this, SLOT( selectTrackPoint( double, double ) ) );
 	connect( ui->plotView, SIGNAL( clickedPoint( double, double ) ),
 				this, SLOT( selectTrackPoint( double, double ) ) );

commit 1ebac0270a10939a8806399500091090a172fb98

Fix bug with getting MapProvider URL in MapView
Commit 1759715fc3c890d036cf0e1f7b4eb773dfcf7d28, introduced a bug setting
the MapView URL to a constant value instead of taking the MapProvider's
mapUrl. Fixed this.

Signed-off-by: Jens Lang <jenslang@users.sf.net>
Signed-off-by: Tobias Doerffel <tobias.doerffel@gmail.com>

src/MapView.cpp
@@ -35,7 +35,6 @@ MapView::MapView( QWidget * _parent ) :
 	m_mapProvider( NULL )
 {
 	page()->mainFrame()->addToJavaScriptWindowObject( "mapView", this );
-	setUrl( QUrl( "qrc:/resources/googlemaps.html" ) );
 }
 
 
@@ -52,6 +51,7 @@ MapView::~MapView()
 void MapView::setMapProvider( MapProvider * mapProvider )
 {
 	m_mapProvider = mapProvider;
+	setUrl( mapProvider->mapUrl() );
 }
 
 