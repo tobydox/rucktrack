tobydox/rucktrack

commit cd79ab4127e9d05e2489f475a3a49179aa304ccb

GpxFile::loadRoute(): Ensure timestamps are increasing
I've many (buggy?) tracks where successive trackpoints have identical
timestamps. This leads to nice peaks in the speed profile.
To avoid this, throw away all points with non-increasing timestamps.

Signed-off-by: Tobias Doerffel <tobias.doerffel@gmail.com>

src/GpxFile.cpp
@@ -76,15 +76,25 @@ bool GpxFile::loadRoute( Route & route ) const
 				{
 					timeOffset += qMax<int>( 0, lastPoint.secsTo( TrackPoint( 0, 0, 0, time.addSecs( -timeOffset ) ) ) - 3 );
 				}
-				TrackPoint t(
-					e.attribute( "lat" ).toDouble(),
-					e.attribute( "lon" ).toDouble(),
-					!elev.isEmpty() ?
-						elev.at( 0 ).toElement().text().toDouble() : 0,
-					time.addSecs( -timeOffset ) );
-				trackSeg << t;
-				lastPoint = t;
-				//qDebug() << t.latitude() << t.longitude() << t.elevation() << t.time();
+
+				if( lastPoint.time() < time )
+				{
+					TrackPoint t(
+						e.attribute( "lat" ).toDouble(),
+						e.attribute( "lon" ).toDouble(),
+						!elev.isEmpty() ?
+							elev.at( 0 ).toElement().text().toDouble() : 0,
+						time.addSecs( -timeOffset ) );
+					trackSeg << t;
+					lastPoint = t;
+					//qDebug() << t.latitude() << t.longitude() << t.elevation() << t.time();
+				}
+				else
+				{
+					qWarning( "%s",
+						qPrintable(
+							QObject::tr( "Track point %1 dropped because its time is not later than previous one." ).arg( point + 1 ) ) );
+				}
 			}
 		}
 		route << trackSeg;

commit 1777492ee4e3eea8368479004619a18e1092572f

GpxFile: fixed loading of files without timestamps
Commit cd79ab4127e9d05e2489f475a3a49179aa304ccb broke loading of files
without any timestamps at all. Fix this by checking for time being null.

Signed-off-by: Tobias Doerffel <tobias.doerffel@gmail.com>

src/GpxFile.cpp
@@ -70,14 +70,14 @@ bool GpxFile::loadRoute( Route & route ) const
 					time = QDateTime::fromString(
 							timeNode.at( 0 ).toElement().text(),
 								Qt::ISODate );
-				}
-				// fix gaps between track segments
-				if( seg > 0 && point == 0 && lastPoint.isValid() )
-				{
-					timeOffset += qMax<int>( 0, lastPoint.secsTo( TrackPoint( 0, 0, 0, time.addSecs( -timeOffset ) ) ) - 3 );
+					// fix gaps between track segments
+					if( seg > 0 && point == 0 && lastPoint.isValid() )
+					{
+						timeOffset += qMax<int>( 0, lastPoint.secsTo( TrackPoint( 0, 0, 0, time.addSecs( -timeOffset ) ) ) - 3 );
+					}
 				}
 
-				if( lastPoint.time() < time )
+				if( lastPoint.time() < time || time.isNull() )
 				{
 					TrackPoint t(
 						e.attribute( "lat" ).toDouble(),